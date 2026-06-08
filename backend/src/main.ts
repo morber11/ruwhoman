5f25d5f8de683ea641daa644d367f9afd42e1999
morber <4497491+morber11@users.noreply.github.com>
2026-06-08 16:02:07 +0100

    add helmet

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
+    app.use(helmet());
     app.setGlobalPrefix('api');
     app.enableCors({ origin: process.env.FRONTEND_URL });
     app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
